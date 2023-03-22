16fb7d4ab5510a36b940834235773487a383a34c
webdev-ashishk <webdev.ashishk@gmail.com>
2023-03-22 08:22:06 +0530

    episode-3 && episode-4 completed

---
+++ hoisting-in-javascript-episode-3/javascript.js
@@ -0,0 +1,18 @@
+
+
+var x = 10;
+
+function getName() {
+  console.log("Namaste javascript");
+}
+getName();
+console.log(x);
+console.log(getName);
+
+// getName and getName2 behave like variable
+// var getName2 = function () {
+//   console.log("name2 called");
+// };
+// var getName1 = () => {
+//   console.log("getName called");
+// };
