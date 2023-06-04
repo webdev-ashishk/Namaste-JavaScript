bdfc5543aed53dadb6f38ea42369f1ba9cd1ca3c
webdev-ashishk <webdev.ashishk@gmail.com>
2023-06-04 13:40:26 +0530

    added setimeout

---
+++ setTimeout and Closure interview episode-11/index.js
@@ -0,0 +1,19 @@
+/*
+1] some developer understant that first wait for 3 second and print 1 and Namaste javascript but they are wrong becoz javascipt doesn't wait for any one!
+2]This code will print Namste javascript first and after that print 1;
+*/
+
+// function x() {
+//   var a = 1;
+//   setTimeout(function () {
+//     console.log(a);
+//   }, 3000);
+//   console.log("Namste javascript");
+// }
+// x();
+
+
+/*  */
+
+
+
