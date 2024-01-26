49ef6854f3a141b196b89b6208384175f5514a43
webdev-ashishk <webdev.ashishk.com>
2024-01-26 22:32:49 +0530

    added custom error message using the throw keywords

--- error-handling-by-anju/index.js
+++ error-handling-by-anju/index.js
@@ -1,9 +1,23 @@
 try {
-  console.log(age);
+  const resp = prompt("check resp");
+  if (resp === "resp") {
+    console.log("resp printed");
+  } else {
+    console.log("error in response");
+    throw new Error("❓ this is the custom error message ");
+  }
 } catch (error) {
-  console.error(
-    `error name ${error.name} and error message is ${error.message}`
+  console.log(
+    `error name is ${error.name} and error message is ${error.message}`
   );
 }
-var str = "very important works are here !";
-console.log({ str });
+
+// try {
+//   console.log(age);
+// } catch (error) {
+//   console.error(
+//     `error name ${error.name} and error message is ${error.message}`
+//   );
+// }
+// var str = "very important works are here !";
+// console.log({ str });
